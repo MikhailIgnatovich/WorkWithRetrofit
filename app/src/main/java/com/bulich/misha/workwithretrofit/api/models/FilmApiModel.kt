adf2733ef85aa01efa1371589fbb8c7b8f3a5189
MikhailIgnatovich <bulich198922@gmail.com>
2021-07-31 18:49:23 +0300

    Добавил GenreApiModel.kt и FilmApiModel.kt

---
+++ app/src/main/java/com/bulich/misha/workwithretrofit/api/models/FilmApiModel.kt
@@ -0,0 +1,9 @@
+package com.bulich.misha.workwithretrofit.api.models
+
+data class FilmApiModel(
+    var id: Int? = null,
+    var name: String? = null,
+    var genre: String? = null,
+    var rating: String? = null,
+    var duration: String? = null
+)
---
+++ app/src/main/java/com/bulich/misha/workwithretrofit/api/models/GenreApiModel.kt
@@ -0,0 +1,6 @@
+package com.bulich.misha.workwithretrofit.api.models
+
+data class GenreApiModel(
+    var id: Int? = null,
+    var name: String? = null
+)

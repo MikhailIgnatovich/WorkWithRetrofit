180061707360ed821c3fb97d7f033430bfc2d193
MikhailIgnatovich <bulich198922@gmail.com>
2021-08-05 22:54:11 +0300

    добавил CatalogGenres.kt и PanelEditGenre.kt

--- app/src/main/java/com/bulich/misha/workwithretrofit/tabs/genres/CatalogGenres.kt
+++ app/src/main/java/com/bulich/misha/workwithretrofit/tabs/genres/CatalogGenres.kt
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bulich.misha.workwithretrofit.R
+import com.bulich.misha.workwithretrofit.api.ApiClient
+import com.bulich.misha.workwithretrofit.api.models.GenreApiModel
+import com.bulich.misha.workwithretrofit.databinding.CatalogGenresBinding
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class CatalogGenres : Fragment() {
+
+    private var binding: CatalogGenresBinding? = null
+    private var genresAdapter: GenresAdapter? = null
 
 
     override fun onCreateView(
@@ -16,6 +30,89 @@ class CatalogGenres : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.catalog_genres, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.catalog_genres, container, false)
+
+        loadGenres()
+
+        binding?.deleteAllGenre?.setOnClickListener {
+            clearAllGenres()
+        }
+
+        return binding?.root
+    }
+
+
+    private fun clearAllGenres() {
+        val callClearAllGenres: Call<ResponseBody?>? = ApiClient.instance?.api?.clearGenre()
+
+        callClearAllGenres?.enqueue(object : Callback<ResponseBody?> {
+            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
+                Toast.makeText(context, "Жанры удалены!", Toast.LENGTH_LONG).show()
+                loadGenres()
+            }
+
+            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+                Toast.makeText(context, "Ошибка! Включите Интернет", Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+    }
+
+
+    private fun loadGenres() {
+        val callGenres = ApiClient.instance?.api?.getGenre()
+
+        callGenres?.enqueue(object : Callback<ArrayList<GenreApiModel>> {
+            override fun onResponse(
+                call: Call<ArrayList<GenreApiModel>>,
+                response: Response<ArrayList<GenreApiModel>>
+            ) {
+                val loadGenres = response.body()
+                binding?.recyclerGenres?.layoutManager = LinearLayoutManager(context)
+                genresAdapter = loadGenres?.let {
+                    GenresAdapter(
+                        it, { idGenre: Int -> deleteGenre(idGenre) },
+                        { genreApiModel: GenreApiModel -> editGenre(genreApiModel) }
+                    )
+                }
+                binding?.recyclerGenres?.adapter = genresAdapter
+
+                Toast.makeText(context, "ЗАГРУЗКА", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call<ArrayList<GenreApiModel>>, t: Throwable) {
+                Toast.makeText(context, "Ошибка! Включите Интернет!", Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
+
+
+    private fun editGenre(genreApiModel: GenreApiModel) {
+        val panelEditGenre = PanelEditGenre()
+        val parameters = Bundle()
+        parameters.putString("idGenre", genreApiModel.id.toString())
+        parameters.putString("nameGenre", genreApiModel.name)
+        panelEditGenre.arguments = parameters
+
+        panelEditGenre.show((context as FragmentActivity).supportFragmentManager, "editGenre")
+    }
+
+
+    private fun deleteGenre(idGenre: Int) {
+        val callDeleteGenre: Call<ResponseBody?>? = ApiClient.instance?.api?.deleteGenre(idGenre)
+
+        callDeleteGenre?.enqueue(object : Callback<ResponseBody?> {
+            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
+                Toast.makeText(context, "Жанр удален!", Toast.LENGTH_LONG).show()
+                loadGenres()
+            }
+
+            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+                Toast.makeText(context, "Ошибка! Включите Интернет", Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
--- app/src/main/java/com/bulich/misha/workwithretrofit/tabs/genres/PanelEditGenre.kt
+++ app/src/main/java/com/bulich/misha/workwithretrofit/tabs/genres/PanelEditGenre.kt
@@ -1,14 +1,27 @@
 package com.bulich.misha.workwithretrofit.tabs.genres
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import com.bulich.misha.workwithretrofit.R
+import com.bulich.misha.workwithretrofit.api.ApiClient
+import com.bulich.misha.workwithretrofit.databinding.PanelEditGenreBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
 
 
-class PanelEditGenre : Fragment() {
+class PanelEditGenre : BottomSheetDialogFragment(), View.OnKeyListener {
+
+    private var binding: PanelEditGenreBinding? = null
+    private var idGenre: Int? = null
 
 
     override fun onCreateView(
@@ -16,6 +29,51 @@ class PanelEditGenre : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.panel_edit_genre, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.panel_edit_genre, container, false)
+
+        idGenre = arguments?.getString("idGenre")?.toInt()
+        binding?.editGenre?.setText(arguments?.getString("nameGenre").toString())
+
+        binding?.editGenre?.setOnKeyListener(this)
+
+        return binding?.root
+    }
+
+    override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+        when (v.id) {
+            R.id.editGenre -> {
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    updateGenre(binding?.editGenre?.text.toString())
+
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+
+    private fun updateGenre(name: String) {
+        val callUpdateGenre = ApiClient.instance?.api?.updateGenre(idGenre.toString().toInt(), name)
+        callUpdateGenre?.enqueue(object : retrofit2.Callback<ResponseBody?> {
+            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
+                Toast.makeText(context, "Жанр обновлен!", Toast.LENGTH_LONG).show()
+                loadScreen()
+            }
+
+            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+                Toast.makeText(context, "Ошибка! Включите интернет!", Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
+
+    private fun loadScreen() {
+        binding?.editGenre?.setText("")
+
+        dismiss()
+
+        (context as FragmentActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.content, CatalogGenres())
     }
 }
